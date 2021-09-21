9a40f6efc8aa1d88c197f24ef0cc11338b747478
limonikas@me.com <limonikas@me.com>
2021-09-22 02:50:34 +0300

    Core data code cleanup

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Cat on 22.09.2021.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+
+ let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
---
+++ Devote/View/ContentView.swift
@@ -0,0 +1,90 @@
+//
+//  ContentView.swift
+//  Devote
+//
+//  Created by Cat on 22.09.2021.
+//
+
+import CoreData
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTIES
+
+    @Environment(\.managedObjectContext) private var viewContext
+
+    // FETCHING DATA
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        animation: .default)
+    private var items: FetchedResults<Item>
+
+    // MARK: - FUNCTION
+
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(context: viewContext)
+            newItem.timestamp = Date()
+
+            do {
+                try viewContext.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    // MARK: - BODY
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(items) { item in
+                    NavigationLink {
+                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                    } label: {
+                        Text(item.timestamp!, formatter: itemFormatter)
+                    }
+                }
+                .onDelete(perform: deleteItems)
+            } //: LIST
+            .toolbar {
+                #if os(iOS)
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                #endif
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            } //: TOOLBAR
+            Text("Select an item")
+        }
+    }
+}
+
+
+
+
+// MARK: - PREVIEW
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
